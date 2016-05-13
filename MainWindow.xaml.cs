e7f5e98e31d4f8f02a45b9bebc47c58d31fbecfd
NoxCaos <glebx95@gmail.com>
2016-05-13 20:41:02 +0300

    Added final components

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,7 +9,10 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
 
 namespace YandereSimLauncher {
     /// <summary>
@@ -17,19 +20,32 @@ namespace YandereSimLauncher {
     /// </summary>
     public partial class MainWindow : Window {
 
+        private const string BLOG_LINK = "http://yanderesimulator.com/";
         private const string VERSION_URL = "http://yanderesimulator.com/version.txt";
         private const string ZIP_URL = "http://yanderesimulator.com/latest.zip";
+        private const string NEWS_URL = "https://public-api.wordpress.com/rest/v1.1/sites/yanderedev.wordpress.com/posts/";
         private const string ZIP_NAME = "content.zip";
+
+        private static Dictionary<string, string> SocialLinks = new Dictionary<string, string>() {
+            { "wordpress",  "http://yanderedev.wordpress.com/" },
+            { "youtube",    "https://www.youtube.com/channel/UC1EBJfK7ltjYUFyzysKxr1g" },
+            { "twitter",    "https://twitter.com/YandereDev" },
+            { "patreon",    "https://www.patreon.com/YandereDev?ty=h" },
+            { "tumblr",     "http://yanderedev.tumblr.com/" }
+        };
 
         private WebClient webClient;
         private string gamePath;
+        private int newGameVersion;
+
+        //private List<Post> posts;
 
         public MainWindow() {
             InitializeComponent();
             webClient = new WebClient();
             gamePath = AppDomain.CurrentDomain.BaseDirectory;
             /*AppDomain.CurrentDomain.UnhandledException += (s, e) => {
-                MessageBox.Show("Unexpected error happend. If you see this window second time, please email to 'gleb.noxcaos@gmail.com' and attach 'launcherCrashDump.txt that is next to this executable''", "Fatal error");
+                MessageBox.Show("If you see this window second time, please report a bug on 'http://yanderesimulator.com/Report.html' and attach 'launcherCrashDump.txt that is next to this executable''", "Fatal error");
                 using (var writer = new StreamWriter(AppDomain.CurrentDomain.BaseDirectory + "launcherCrashDump.txt", true)) {
                     writer.WriteLine(DateTime.Today.ToShortDateString() + " at " + DateTime.Today.ToShortTimeString());
                     writer.WriteLine(e.ExceptionObject.ToString());
@@ -39,6 +55,7 @@ namespace YandereSimLauncher {
             };*/
         }
 
+        #region Events
         private void WindowMouseDown(object sender, MouseButtonEventArgs e) {
             try {
                 DragMove();
@@ -53,6 +70,28 @@ namespace YandereSimLauncher {
             WindowState = WindowState.Minimized;
         }
 
+        private void OnSocialButtonClick(object sender, MouseButtonEventArgs e) {
+            var netw = ((Image)sender).Name.Split('_')[1];
+            Process.Start(SocialLinks[netw]);
+        }
+
+        private void OnPlayButtonClick(object sender, RoutedEventArgs e) {
+            Process.Start(gamePath + "YandereSimulator.exe");
+            this.Close();
+        }
+
+        private void OnRedownloadClick(object sender, RoutedEventArgs e) {
+            try {
+                RedownloadButton.IsEnabled = false;
+                PlayButton.IsEnabled = false;
+                CleanUp();
+                Thread t = new Thread(StartCheckingInternetConnection);
+                t.Start();
+            } catch { }
+        }
+        #endregion
+
+        #region Static
         public static string GetData(string url) {
             ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
             WebRequest reqGET = WebRequest.Create(url);
@@ -63,18 +102,142 @@ namespace YandereSimLauncher {
             return html;
         }
 
-        public static int BytesToMega(long bytes) {
-            return (int)bytes / 1000000;
+        public static void DeleteDirectory(string target_dir) {
+            string[] files = Directory.GetFiles(target_dir);
+            string[] dirs = Directory.GetDirectories(target_dir);
+
+            foreach (string file in files) {
+                File.SetAttributes(file, FileAttributes.Normal);
+                File.Delete(file);
+            }
+
+            foreach (string dir in dirs) {
+                DeleteDirectory(dir);
+            }
+
+            Directory.Delete(target_dir, false);
         }
 
+        public static string BytesToMega(long bytes) {
+            return (bytes / 1000000.0).ToString("0.00");
+        }
+        #endregion
+
+        #region Animations
+        private void OnButtonHover(object sender, MouseEventArgs e) {
+            ScaleTransform rt = new ScaleTransform();
+            Image obj = (Image)sender;
+            obj.RenderTransform = rt;
+            obj.RenderTransformOrigin = new Point(0.5, 0.5);
+            DoubleAnimation da = new DoubleAnimation(1, 1.25, new Duration(TimeSpan.FromSeconds(0.2)));
+            da.FillBehavior = FillBehavior.HoldEnd;
+
+            rt.BeginAnimation(ScaleTransform.ScaleXProperty, da);
+            rt.BeginAnimation(ScaleTransform.ScaleYProperty, da);
+        }
+
+        private void OnButtonLeave(object sender, MouseEventArgs e) {
+            ScaleTransform rt = new ScaleTransform();
+            Image obj = (Image)sender;
+            obj.RenderTransform = rt;
+            obj.RenderTransformOrigin = new Point(0.5, 0.5);
+            DoubleAnimation da = new DoubleAnimation(1.25, 1, new Duration(TimeSpan.FromSeconds(0.2)));
+            da.FillBehavior = FillBehavior.Stop;
+
+            rt.BeginAnimation(ScaleTransform.ScaleXProperty, da);
+            rt.BeginAnimation(ScaleTransform.ScaleYProperty, da);
+        }
+
+        private void OnTextLeave(object sender, MouseEventArgs e) {
+            var block = (TextBlock)sender;
+            block.Foreground = new SolidColorBrush(Color.FromArgb(255, 255, 125, 205));
+        }
+
+        private void OnTextEnter(object sender, MouseEventArgs e) {
+            var text = (TextBlock)sender;
+            text.Foreground = new SolidColorBrush(Color.FromArgb(255, 62, 62, 62));
+        }
+
+        private void OnPostTitleClick(object sender, MouseButtonEventArgs e) {
+            var block = (TextBlock)sender;
+            block.Foreground = new SolidColorBrush(Color.FromArgb(255, 255, 125, 205));
+            //var link = posts.Where(p => p.Title == block.Text).FirstOrDefault().URL;
+            //Process.Start(link);
+        }
+
+        private void OnHeadTitleClick(object sender, MouseButtonEventArgs e) {
+            try {
+                var block = (TextBlock)sender;
+                var parts = block.Name.Split('_');
+                if (parts.Length == 2) {
+                    Process.Start(BLOG_LINK + parts[1]);
+                } else Process.Start(BLOG_LINK + parts[1] + "-" + parts[2]);
+            } catch (ArgumentOutOfRangeException) { }
+        }
+        #endregion
+
+        #region News
+        private void GetNews() {
+            var data = GetData(NEWS_URL);
+            //var resp = WordpressResponse.Parse(data);
+            //this.posts = resp.posts;
+
+            Dispatcher.Invoke(new Action(() => {
+                for (int i=0; i<5; i++) {
+                    //PostContainer.Children.Add(MakeDateTextBlock(resp.posts[i].Date));
+                    //PostContainer.Children.Add(MakeTitleTextBlock(resp.posts[i].Title));
+                }
+            }));
+        }
+
+        private TextBlock MakeTitleTextBlock(string title) {
+            var block = new TextBlock();
+            block.Text = title;
+            block.Foreground = new SolidColorBrush(Color.FromArgb(255, 255, 125, 205));
+            block.FontSize = 16;
+            block.FontFamily = new FontFamily("Segoe UI Semibold");
+            block.Cursor = Cursors.Hand;
+            block.Margin = new Thickness(0, 0, 0, 10);
+            block.MouseEnter += OnTextEnter;
+            block.MouseLeave += OnTextLeave;
+            block.MouseDown += OnPostTitleClick;
+            return block;
+        }
+
+        private TextBlock MakeDateTextBlock(DateTime date) {
+            var block = new TextBlock();
+            block.Text = date.DayOfWeek + ", " + date.Date.ToShortDateString();
+            block.Foreground = new SolidColorBrush(Color.FromArgb(255, 62, 62, 62));
+            block.FontSize = 11;
+            block.FontFamily = new FontFamily("Segoe UI Semibold");
+            return block;
+        }
+
+        private string GetImageUrl(string content) {
+            if(content.Contains("<img src=\"")) {
+                return content.Split(
+                    new string[] { "<img src=\"" }, StringSplitOptions.None)[1]
+                    .Split('\"')[0];
+            }
+            return string.Empty;
+        }
+        #endregion
+
+        #region Logic
         private bool IsGameUpToDate() {
             try {
-                var executable = Directory.EnumerateFiles(gamePath, "YandereSim*.exe").ElementAt(0);
-                var content = Directory.EnumerateDirectories(gamePath, "YandereSim*_Data").ElementAt(0);
                 var serverVersion = GetData(VERSION_URL);
-                var clientVersion = File.ReadAllText(content + "version");
-                Debug.WriteLine(serverVersion);
-                if (int.Parse(serverVersion) > int.Parse(clientVersion)) {
+                newGameVersion = int.Parse(serverVersion);
+            } catch {
+                
+            }
+
+            try {
+                var executable = Directory.EnumerateFiles(gamePath, "YandereSimulator.exe").ElementAt(0);
+                var content = Directory.EnumerateDirectories(gamePath, "YandereSimulator_Data").ElementAt(0);
+                var clientVersion = File.ReadAllText(content + "\\version");
+
+                if (newGameVersion > int.Parse(clientVersion)) {
                     CleanUp();
                     return false;
                 } else return true;
@@ -85,10 +248,10 @@ namespace YandereSimLauncher {
         }
 
         private void CleanUp() {
-            var executable = Directory.EnumerateFiles(gamePath, "YandereSim*.exe");
-            var content = Directory.EnumerateDirectories(gamePath, "YandereSim*_Data");
+            var executable = Directory.EnumerateFiles(gamePath, "YandereSimulator.exe");
+            var content = Directory.EnumerateDirectories(gamePath, "YandereSimulator_Data");
             if (executable.Any()) File.Delete(executable.ElementAt(0));
-            if (content.Any()) Directory.Delete(content.ElementAt(0));
+            if (content.Any()) DeleteDirectory(content.ElementAt(0));
         }
 
         private void DownloadGameContents() {
@@ -100,17 +263,38 @@ namespace YandereSimLauncher {
 
         private void DownloadDataCompleted(object sender, AsyncCompletedEventArgs e) {
             CleanUp();
+            webClient.DownloadProgressChanged -= new DownloadProgressChangedEventHandler(UpdateProgressBar);
+            webClient.DownloadFileCompleted -= new AsyncCompletedEventHandler(DownloadDataCompleted);
 
-            using (var arc = ZipFile.Read(gamePath + ZIP_NAME)) {
-                arc.ExtractAll(gamePath, ExtractExistingFileAction.OverwriteSilently);
+            GetData("http://simplehitcounter.com/hit.php?uid=2101053&f=16777215&b=0");
+            ReportStatus("Extracting...");
+            try {
+                using (var arc = ZipFile.Read(gamePath + ZIP_NAME)) {
+                    arc.ExtractAll(gamePath, ExtractExistingFileAction.OverwriteSilently);
+                }
+                File.Delete(gamePath + ZIP_NAME);
+            } catch {
+                ReportStatus("Can't finish extracting");
+                Dispatcher.Invoke(new Action(() => {
+                    RedownloadButton.IsEnabled = true;
+                    PlayButton.IsEnabled = false;
+                }));
             }
-            File.Delete(gamePath + ZIP_NAME);
-
+            try {
+                File.WriteAllText(gamePath + "YandereSimulator_Data\\" + "version", newGameVersion.ToString());
+            } catch {
+                Dispatcher.Invoke(new Action(() => {
+                    RedownloadButton.IsEnabled = true;
+                    PlayButton.IsEnabled = false;
+                }));
+                MessageBox.Show("Validation error happend while installing. However, the game was successfully downloaded and extracted. You can try starting it manually", "Version validator");
+                Process.Start(gamePath);
+            }
             SetReadyStatus();
         }
 
         private void UpdateProgressBar(object sender, DownloadProgressChangedEventArgs e) {
-            ProgressBar.Value = e.ProgressPercentage;
+            ReportProgress(e.ProgressPercentage);
             ReportStatus(string.Format("Downloaded {0}mb of {1}mb", 
                 BytesToMega(e.BytesReceived),
                 BytesToMega(e.TotalBytesToReceive)
@@ -127,16 +311,53 @@ namespace YandereSimLauncher {
             }
         }
 
+        private void StartCheckingInternetConnection() {
+            ReportStatus("Checking server connection...");
+            try {
+                GetData(BLOG_LINK);
+                Thread t = new Thread(StartCheckThread);
+                t.Start();
+            } catch (WebException) {
+                ReportStatus("Can't connect to update server");
+                try {
+                    var executable = Directory.EnumerateFiles(gamePath, "YandereSimulator.exe").ElementAt(0);
+                    var content = Directory.EnumerateDirectories(gamePath, "YandereSimulator_Data").ElementAt(0);
+                    Dispatcher.Invoke(new Action(() => {
+                        PlayButton.IsEnabled = true;
+                    }));
+                } catch {
+                    Dispatcher.Invoke(new Action(() => {
+                        RedownloadButton.IsEnabled = false;
+                        PlayButton.IsEnabled = false;
+                    }));
+                }
+                Thread.Sleep(5000);
+                Thread t = new Thread(StartCheckingInternetConnection);
+                t.Start();
+            }
+        }
+
+        private void ReportProgress(int progress) {
+            Dispatcher.Invoke(new Action(() => {
+                ProgressBar.Value = progress;
+            }));
+        }
+
         private void ReportStatus(string status) {
-            Dispatcher.Invoke(new Action(() => { DownloadStatus.Text = status; }));
+            Dispatcher.Invoke(new Action(() => {
+                DownloadStatus.Text = status;
+            }));
         }
 
         private void SetReadyStatus() {
             Dispatcher.Invoke(new Action(() => {
                 PlayButton.IsEnabled = true;
-                DownloadStatus.Text = "Ready";
+                RedownloadButton.IsEnabled = true;
+                DownloadStatus.Text = "Game is up-to-date";
+                ProgressBar.Value = 100;
             }));
         }
+        #endregion
 
         /// <summary>
         /// MAIN PROCEDURE
@@ -144,8 +365,12 @@ namespace YandereSimLauncher {
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void OnWindowLoaded(object sender, RoutedEventArgs e) {
-            Thread t = new Thread(StartCheckThread);
+            RedownloadButton.IsEnabled = false;
+            PlayButton.IsEnabled = false;
+            Thread t = new Thread(StartCheckingInternetConnection);
             t.Start();
+            //Thread n = new Thread(GetNews);
+            //n.Start();
         }
     }
 }
---
+++ WordpressResponse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//using Newtonsoft.Json;
+
+namespace YandereSimLauncher {
+    /*public class WordpressResponse {
+        public int found;
+        public List<Post> posts;
+
+        public static WordpressResponse Parse(string json) {
+            return JsonConvert.DeserializeObject<WordpressResponse>(json);
+        }
+    }
+
+    public class Post {
+        [JsonProperty("ID")]
+        public int ID;
+
+        [JsonProperty("date")]
+        public DateTime Date;
+
+        [JsonProperty("title")]
+        public string Title;
+
+        [JsonProperty("URL")]
+        public string URL;
+
+        [JsonProperty("content")]
+        public string Content;
+    }*/
+}
